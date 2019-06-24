3307aba9cdc8d26dea3cb5fbd031e01901409917
Rafael Ramos <arkanjo.ms@gmail.com>
2019-06-23 22:54:11 -0300

    fixing material-ui deprecations

--- src/Commons.jsx
+++ src/Commons.jsx
@@ -71,9 +71,9 @@ const styles = theme => ({
             easing: theme.transitions.easing.sharp,
             duration: theme.transitions.duration.leavingScreen,
         }),
-        width: theme.spacing.unit * 7,
+        width: theme.spacing(7),
         [theme.breakpoints.up('sm')]: {
-            width: theme.spacing.unit * 9,
+            width: theme.spacing(9),
         },
     },
     grow: {
@@ -89,7 +89,7 @@ const styles = theme => ({
     content: {
         flexGrow: 1,
         backgroundColor: theme.palette.background.default,
-        padding: theme.spacing.unit * 3,
+        padding: theme.spacing(3),
     },
     sectionDesktop: {
         display: 'none',
